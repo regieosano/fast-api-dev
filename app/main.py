470a11bc1cb65d9f4d87aa2d689f29ccef8cd197
Reginald Osano <regie30developer@gmail.com>
2023-12-21 06:23:15 +0800

    edited main.py

--- app/main.py
+++ app/main.py
@@ -7,10 +7,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 init_db()
