7c77d599dc0cee1b386e37c4217d0340321b2ebd
Reginald Osano <regie30developer@gmail.com>
2023-12-23 05:24:22 +0800

    edited removing alembic/env.py db startup

--- db/database.py
+++ db/database.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from config.config_settings import settings
+import psycopg2
 
 ssl_string = "ssl=true" 
 
 SQL_ALCHEMY_DB_URL = f"postgresql://{settings.user}:{settings.password}@{
     settings.host}:{settings.port}/{settings.database_name}"
+
+conn = psycopg2.connect(SQL_ALCHEMY_DB_URL, sslmode='require')
+
+conn.autocommit = True
 
 Base = declarative_base()
 
